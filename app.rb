42c644bb8459d54217c349d41fd9f8ed6892d44d
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-11-30 18:26:02 -0500

    URL params are now named

--- app.rb
+++ app.rb
@@ -5,15 +5,14 @@ get '/' do
   erb :index
 end
 
-get '/:name' do
-  @name = params[:name]
-  @pastes = User.first_or_create(name: @name).pastes # if just created, @pastes should be [], user .saved? to check for creation
+get '/:name' do |name|
+  @pastes = User.first_or_create(name: name).pastes # if just created, @pastes should be [], user .saved? to check for creation
   erb :pastes
 end
 
-post '/:name' do
-  @name = params[:name]
-  @p = User.first_or_create(name: @name).pastes.create(content: params[:paste])
+post '/:name' do |name|
+  @p = User.first_or_create(name: name).pastes.create(content: params[:paste])
+  # should either return json or render :pastes depending on incoming format
   content_type :json
   @p.to_json
 end
